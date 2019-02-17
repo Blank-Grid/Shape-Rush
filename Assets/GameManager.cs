5b61f016da89b09542b3d7794d4900bf8acd5b4a
Blank-Grid <578956771@qq.com>
2019-02-18 00:07:03 +0800

    add GameManager fuction (unfinished)

---
+++ Assets/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Xml;
+
+public class GameManager : MonoBehaviour {
+    private bool isGameOver = false;
+    private int score = 0;
+    public Text scoreText;
+    public Text lifeText;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
