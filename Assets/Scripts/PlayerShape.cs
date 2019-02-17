04f6764efee173ea4104d79b0b403e427a87e005
Blank-Grid <578956771@qq.com>
2019-02-17 20:04:36 +0800

    add annotation

--- Assets/Scripts/PlayerShape.cs
+++ Assets/Scripts/PlayerShape.cs
@@ -6,17 +6,17 @@ public class PlayerShape : MonoBehaviour {
     // 私有属性
     private float[] realPos = { -1.1f, 0, 1.1f }; // 三个位置的x坐标
     private string[] pos = { "left", "middle", "right" }; // 三个位置
-    private string currentPos; // 几何体当前位置
-    private int currentPosIndex; // 几何体当前位置抽象数字
+    private string currentPos; // 几何体当前位置(left,middle,right)
+    private int currentPosIndex; // 几何体当前位置抽象数字(left=0,middle=1,right=2)
     private float speed = 4f; // 几何体移动速度
-    private int direction; // 方向
+    private int direction; // 方向(left=-1,nothing=0,right=1)
 
 
 	// Use this for initialization
 	void Start () {
         // 位置初始化
-        currentPos = "middle";
-        currentPosIndex = 1;
+        currentPos = "left";
+        currentPosIndex = 0;
 	}
 	
 	// Update is called once per frame
